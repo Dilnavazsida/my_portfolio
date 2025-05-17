729afb6a734deeaf774141159a9a66393f58087f
Dilnavazsida <navazdil07@gmail.com>
2025-05-17 11:06:32 +0530

    Add files via upload

---
+++ about.blade.php
@@ -0,0 +1,102 @@
+@php
+    include('C:\xampp\htdocs\LaravelProjects\portfolio\resources\views\navbar.blade.php');
+@endphp
+
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- ✅ Add FontAwesome for social media icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    </head>
+    <body style="background-color:#97D2FB;">
+    <table  class=" tbl-1 bg-dark">
+    <tr>
+        <th colspan="4"><center>Education Details</center></th>
+    </tr>
+
+    <tr>
+        <th>Degree</th>
+        <th>Institution</th>
+        <th>Year</th>
+        <th>Details</th>
+    </tr>
+
+    <tr>
+        <td>10<sub>th</sub></td>
+        <td>VALI-E-E-SORATH HIGH SCHOLL-JUNAGADH</td>
+        <td>MARCH-2019</td>
+        <td>Scored 65% in GSEB bord Exams.</td>
+    </tr>
+
+    <tr>
+        <td>12<sub>th</sub></td>
+        <td>SHREE VIVEKANAND VIDYABHAVAN HIGHER SECONDARY- <br> SCHOLL-JUNAGADH</td>
+        <td>MAY-2021</td>
+        <td>Scored 71.56% in GSHSEB bord Exams.</td>
+    </tr>
+
+    <tr>
+        <td>BCA</sub></td>
+        <td>DR.SUBHASH COLLAGE OF COMPUTER SCIENCE</td>
+        <td>MARCH-2024</td>
+        <td>Scored 60% in bca</td>
+    </tr>
+
+    <tr>
+        <td>MCA</sub></td>
+        <td>ATMIYA UNIVERSITY</td>
+        <td>PENDING</td>
+        <td>PENDING</td>
+    </tr>
+</table>
+
+<table  class="tbl-2 bg-dark">
+    <tr>
+        <th colspan="2"> <center>My Personal Details</center></th>
+    </tr>
+    <tr>
+        <th>Birth-Date</th>
+        <td>22-6-2003</td>
+    </tr>
+    <tr>
+        <th>Contry</th>
+        <td>india</td>
+    </tr>
+    <tr>
+        <th>State</th>
+        <th>Gujarat</th>
+    </tr>
+    <tr>
+        <th>City</th>
+        <th>Junagadh</th>
+    </tr>
+    <tr>
+        <th>Address</th>
+        <th>Kasturbag Colony Street No 11 Dolatpara Junagadh - 362001</th>
+    </tr>
+</table>
+<div class="footer bg-dark">
+
+        <div class="eml-l-back-h"></div>
+       <a href="https://www.gmail.com/"> <i class="fa-regular fa-envelope eml-l-h"></i></i></a>
+
+       <div class="linkdin-l-back-h"></div>
+      <a href="https://www.linkedin.com/" clas="a-l-clr"><i class="fa-brands fa-linkedin-in linkdn-l-h"></i> </a> 
+
+        <div class="git-l-back-h"></div>
+       <a href="https://github.com/Dilnavazsida/Web_programing_php"><i class="fa-brands fa-github git-l-h" ></i></a> 
+
+        <div class="insta-l-back-h"></div>
+       <a href="https://www.instagram.com/dilnavaz_sida/?next=%2F"><i class="fa-brands fa-instagram insta-l-h"></i></a> 
+
+        <a href="/home" class="hm-link">Go To HOME PAGE</a>
+        <a href="/about" class="ab-link">Go To ABOUT PAGE</a>
+        <a href="/work" class="wk-link">Go To WORK PAGE</a>
+        <a href="/skill" class="sk-link"> Go To SKILL PAGE</a>
+        <a href="/contact" class="ct-link">Go To  CONTACT PAGE</a>
+        <p style="color:#0ef; font-size:10px position:absolute;margin-left:1330px;margin-top:150px">Design By ❤️Navaz</p>
+</div>
+
+
+    </body>
+</html>
---
+++ contact.blade.php
@@ -0,0 +1,89 @@
+@php
+    include('C:\xampp\htdocs\LaravelProjects\portfolio\resources\views\navbar.blade.php');
+@endphp
+
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- ✅ Add FontAwesome for social media icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    </head>
+    <body style="background-color:#97D2FB;">
+    <h1 class="contct bg-dark"><p style="position:absolute;margin-top:10px;margin-left:7px;">My Contact Details</p></h1>
+   
+   <!-- <div class="main-div-che" style="position:relative; width:1250px; background-color: aliceblue;"> -->
+   
+       <div class="con-main bg-dark">
+           <h1 class="cnm">Contact Me</h1>
+           <h3 class="eml">Email :</h3>
+           <h3 class="eml-txt">navazdil@gmail.com</h3>
+   
+           <h3 class="phn">Phone :</h3>
+           <h3 class="phn-txt">9870086800</h3>
+   
+           <h3 class="lnkdin">Linkedin :</h3>
+           <h3 class="lnkdin-txt"><a href="https://www.linkedin.com/">Go To Linkedin</a></h3>
+   
+           <h3 class="github">GitHub :</h3>
+           <h3 class="github-txt"><a href="https://github.com/Dilnavazsida/Web_programing_php">Go To GitHub</a></h3>
+   
+           <h3 class="insta">Instagram :</h3>
+           <h3 class="insta-txt"><a href="https://www.instagram.com/dilnavaz_sida/?next=%2F">Go To insta</a></h3>
+   
+           
+           <div class="cr-1"></div>
+           <div class="l-1"></div>
+           <div class="cr-2"></div>
+           <div class="l-2"></div>
+           <div class="cr-3"></div>
+           <div class="l-3"></div>
+           <div class="cr-4"></div>
+           <div class="l-4"></div>
+           <div class="cr-5"></div>
+   
+           <div class="eml-l-back"></div>
+           <i class="fa-regular fa-envelope eml-l"></i>
+   
+          <div class="linkdin-l-back"></div>
+           <i class="fa-brands fa-linkedin linkdn-l"></i> 
+   
+           <div class="git-l-back"></div>
+           <i class="fa-brands fa-github git-l" ></i>
+   
+           <div class="insta-l-back"></div>
+           <i class="fa-brands fa-instagram insta-l"></i>
+   
+           
+        <div class="con-main-2 bg-dark">
+               <h1 class="lctn">Location</h1>
+               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3340.963162439402!2d70.76521217474883!3d22.28236564354626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3959cbbfcdeb3e33%3A0x11a782bf6775a71d!2sAtmiya%20University!5e1!3m2!1sen!2sin!4v1739373476389!5m2!1sen!2sin" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="mp"></iframe>
+       </div>
+       </div>
+   
+       <div class="footer bg-dark">
+   
+           <div class="eml-l-back-h"></div>
+          <a href="https://www.gmail.com/"> <i class="fa-regular fa-envelope eml-l-h"></i></i></a>
+   
+          <div class="linkdin-l-back-h"></div>
+         <a href="https://www.linkedin.com/" clas="a-l-clr"><i class="fa-brands fa-linkedin-in linkdn-l-h"></i> </a> 
+   
+           <div class="git-l-back-h"></div>
+          <a href="https://github.com/Dilnavazsida/Web_programing_php"><i class="fa-brands fa-github git-l-h" ></i></a> 
+   
+           <div class="insta-l-back-h"></div>
+          <a href="https://www.instagram.com/dilnavaz_sida/?next=%2F"><i class="fa-brands fa-instagram insta-l-h"></i></a> 
+   
+           <a href="/home" class="hm-link">Go To HOME PAGE</a>
+           <a href="/about" class="ab-link">Go To ABOUT PAGE</a>
+           <a href="/work" class="wk-link">Go To WORK PAGE</a>
+           <a href="/skill" class="sk-link"> Go To SKILL PAGE</a>
+           <a href="/contact" class="ct-link">Go To  CONTACT PAGE</a>
+           <p style="color:#0ef; font-size:10px position:absolute;margin-left:1330px;margin-top:150px">Design By ❤️Navaz</p>
+   </div>
+   
+    </body>
+</html>
+  
+<!-- </div> -->
+   
