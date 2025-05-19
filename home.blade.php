00068d8148776418d6eadc201571d76d573b2a3d
Dilnavazsida <navazdil07@gmail.com>
2025-05-19 22:29:28 +0530

    Add files via upload

---
+++ home.blade.php
@@ -0,0 +1,70 @@
+@php
+    include('C:\xampp\htdocs\LaravelProjects\portfolio\resources\views\navbar.blade.php');
+@endphp
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- ✅ Add FontAwesome for social media icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    </head>
+    <body style="background-color:#97D2FB;">
+    <div class="hading bg-dark">
+    <img src="assets\my_photo.jpg" class="logo" alt="">
+    <h1 class="name">Hey' i Am Dilnavaz</h1> 
+    <h2 class="nm2"> Welcome To My Portfolio</h2> 
+</div>
+
+<div class="main bg-dark">
+    <h1 class="intro ">Introduction  About My Self :</h1>
+
+    <h1 class="nnm "> My Name is Dilnavaz i am Software Devloper.
+     <br>i am from junagadh. Currently I am Studying <br>
+      MASTER  OF COMPUTER APPLICATION (MCA) in    <br>
+       Atmiya University, Rajkot. <br> <br> 
+       I have Good Relationship With My classmates <br>
+        And My Professors. I Have Good Communication <br> 
+        Skills. I particularly Love Such Subjects like <br> React.js, Laravel , Python And Java.
+        <br><br>
+            i am Strong in Logical Thinking And Oral
+            Expressing. <br> <br> i am Creative And Unique in View 
+             on Certain meters 
+    
+    </h1>
+    <div class="circle-1 all "></div>
+    <div class="line-1 all"></div>
+    <div class="circle-2 all"></div>
+    <div class="line-2 all"></div>
+    <div class="circle-3 all"></div>
+    <div class="line-3 all"></div>
+    <div class="circle-4 all"></div>
+   
+</div>
+
+<div class="footer bg-dark">
+
+        <div class="eml-l-back-h"></div>
+       <a href="https://www.gmail.com/"> <i class="fa-regular fa-envelope eml-l-h"></i></i></a>
+
+       <div class="linkdin-l-back-h"></div>
+      <a href="https://www.linkedin.com/" clas="a-l-clr"><i class="fa-brands fa-linkedin-in linkdn-l-h"></i> </a> 
+
+        <div class="git-l-back-h"></div>
+       <a href="https://github.com/Dilnavazsida/Web_programing_php"><i class="fa-brands fa-github git-l-h" ></i></a> 
+
+        <div class="insta-l-back-h"></div>
+       <a href="https://www.instagram.com/dilnavaz_sida/?next=%2F"><i class="fa-brands fa-instagram insta-l-h"></i></a> 
+
+        <a href="/home" class="hm-link">Go To HOME PAGE</a>
+        <a href="/about" class="ab-link">Go To ABOUT PAGE</a>
+        <a href="/work" class="wk-link">Go To WORK PAGE</a>
+        <a href="/skill" class="sk-link"> Go To SKILL PAGE</a>
+        <a href="/contact" class="ct-link">Go To  CONTACT PAGE</a>
+
+        <p style="color:#0ef; font-size:10px position:absolute;margin-left:1330px;margin-top:150px">Design By ❤️Navaz</p>
+</div>
+
+
+    </body>
+</html>
+
+<!-- <h2 class="port-txt"> Welcome To My Portfolio</h2> -->
---
+++ navbar.blade.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="/assets/css/style.css">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+       <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    </head>
+    <body>
+      
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark nav-clr" style="" >
+    <a class="navbar-brand first" id="fno" href="/home">Home</a>
+ 
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav" >
+      <a class="nav-item nav-link  second" id="fno"  href="/work">Work</a>
+      <a class="nav-item nav-link  third" id="fno" href="/skill">Skill</a>
+      <a class="nav-item nav-link four" id="fno" href="/contact">Contact</a>
+      <a class="nav-item nav-link  five" id="fno" href="/about">About</a>
+    </div>
+  </div>
+</nav>
+
+
+
+    </body>
+</html>
